7318a90efc447b7cc3a75a8b896f2f507fe99e9a
AkashTarade91 <AkashTarade91@gmail.com>
2022-03-28 06:53:17 +0530

    "NavBar completed"

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Home } from './components/Home';
 import { About } from './components/About';
-
+import { Navibar } from './components/Navibar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
+    <>
+    <Navibar/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/about' element={<About/>}></Route>
     </Routes>
+    </>
+    
    
   );
 }
---
+++ src/components/Navibar.js
@@ -0,0 +1,49 @@
+import { NavLink } from "react-router-dom"
+import { Nav, Container, Navbar, NavDropdown } from "react-bootstrap"
+export const Navibar = ()=>{
+    return(
+        
+         <div>
+            <Navbar bg="light" expand="lg">
+               <Container>
+                  <Navbar.Brand href="/">RaktKosh</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                     <Nav className="me-auto">
+                     <Nav.Link href="/">Home</Nav.Link>
+                     <Nav.Link href="/about">About</Nav.Link>
+                     <NavDropdown title="Looking for Blood" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Blood Availability</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.2">Nearby Blood Bank</NavDropdown.Item>
+                        
+                     </NavDropdown>
+                     <NavDropdown title="Want to Donate" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Blood Donation Camps</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.2">Appointment for Blood Bank</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.2">Donor Login</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.2">About Blood Donation</NavDropdown.Item>
+                     </NavDropdown>
+
+                     <NavDropdown title="Blood Bank Login" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Raktkosh Login</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.2">Add Your Bloodbank</NavDropdown.Item>
+                        
+                     </NavDropdown>
+                     </Nav>
+                  </Navbar.Collapse>
+               </Container>
+            </Navbar>
+         {/* {  <nav>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/about'>About</NavLink>
+         </nav> } */}
+        
+         </div>
+    
+    )
+}
