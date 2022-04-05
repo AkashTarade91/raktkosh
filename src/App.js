f6641de5043ba1803e2d14b201be70dad7777372
Shubhamraut26 <shubhamraut3254@gmail.com>
2022-04-05 10:17:48 +0530

    AdminHome added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import { Navibar } from './components/Navibar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BloodAvailability from './components/BloodAvailability';
 import BloodBankAvailability from './components/BloodBankAvailability';
+
 import DonorLogin from './components/donor/DonorLogin';
 import DonorRegister from './components/donor/DonorRegister';
 import DonorAppointment from './components/donor/DonorAppointment';
+
 import BloodStock from './components/bank/BloodStock';
 import CampSchedule from './components/bank/CampSchedule';
 import CampRegister from './components/bank/CampRegister';
 import Login from './components/bank/Login';
 import Register from './components/bank/Register';
 import BankHome from './components/bank/BankHome';
+
+import AdminHome from './components/admin/AdminHome';
+
 import { FAQ } from './components/FAQ';
 import React, { Component } from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -44,15 +49,15 @@ class App extends Component {
         <Route path='/donor/login' element={<DonorLogin/>}></Route>
         <Route path='/donor/register' element={<DonorRegister/>}></Route>
         <Route path='/donor/donorAppointment' element={<DonorAppointment/>}></Route>
+
         <Route path='/bank/campSchedule' element={<CampSchedule/>}></Route> 
         <Route path='/bank/campRegister' element={<CampRegister/>}></Route> 
         <Route path='/bank/bankHome' element={<BankHome/>}></Route> 
-
         <Route path='/bank/bloodStock' element={<BloodStock/>}></Route>
-
-  
         <Route path='/bank/login' element={<Login/>}></Route>
         <Route path='/bank/register' element={<Register/>}></Route>
+
+        <Route path='/admin/adminHome' element={<AdminHome/>}></Route>
       </Routes>
       </>
     )
