7472b1828f6dffd005567bd4d69b9acd00b2057b
AkashTarade91 <AkashTarade91@gmail.com>
2022-04-05 17:53:30 +0530

    "Table Added"

---
+++ src/components/admin/table/columns.js
@@ -0,0 +1,35 @@
+//{"id":1,"first_name":"Dmitri","last_name":"Skones","email":"dskones0@washington.edu","date_of_birth":"5/22/2021","age":30,"country":"Indonesia","phone":"6506401277"},
+
+export const COLUMNS=[
+    {
+        Header:'Id',
+        Footer:'Id',
+        accessor:'id'
+    },
+    {
+        Header:'First Name',
+        Footer:'First Name',
+        accessor:'first_name'
+    },
+    {
+        Header:'Last Name',
+        Footer:'Last Name',
+        accessor:'last_name'
+    },
+    {
+        Header:'Date of Birth',
+        Footer:'Date of Birth',
+        accessor:'date_of_birth'
+    },
+    {
+        Header:'Country',
+        Footer:'Country',
+        accessor:'country'
+    },
+    {
+        Header:'Phone',
+        Footer:'Phone',
+        accessor:'phone'
+    }
+
+]
